4f36e98d8b0eeb569a166bd87320c5f878ccd01a
Ashish Kumar <ashishkummar@gmail.com>
2019-11-07 11:08:52 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -2,10 +2,19 @@ import React, { Component } from "react";
 import ReactDOM from 'react-dom';
 import View360Three from "./View360Three"
 
-ReactDOM.render(<View360Three image="https://threejs.org/examples/textures/2294472375_24a3b8ef46_o.jpg"
+ReactDOM.render(<>
 
-
-
-/>, document.getElementById('root'));
-
+<View360Three image="//live.staticflickr.com/4409/36493102601_1bbc5640c4_h.jpg"
+autoRotate="true"
+width="700"
+height="350"
+/>
+<br/>
+<View360Three image="//live.staticflickr.com/4610/39823653582_0d8e0ef4c0_h.jpg"
+autoRotate="true"
+width="300"
+height="250"
+/>
+</>, document.getElementById('root'));
+ 
  
