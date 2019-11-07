235fd3c3655d3e0e85fa97cd0a15e5ae2365c3c3
ashishkumar-xx <ashishkummar@gmail.com>
2019-11-07 09:51:13 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-360image-view-threejs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-360image-view-threejs)
---
+++ index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react"; 
+import ReactDOM from 'react-dom';
+import View360Three from "./View360Three"
+
+ReactDOM.render(<View360Three image="https://threejs.org/examples/textures/2294472375_24a3b8ef46_o.jpg"
+
+
+
+/>, document.getElementById('root'));
+
+ 
